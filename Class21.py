a84c33dc63b346b4ed07cc737c6f49ffcbe80dad
Chats1999 <58978045+Chats1999@users.noreply.github.com>
2020-01-14 19:39:33 +0530

    Add files via upload

--- Class21.py
+++ Class21.py
@@ -1,25 +1,51 @@
-'''
-a=input('enter a name')
-b=input('enter the second name')
-d=['Friends','Love','Affection','Marriage','Enemy','Sibling']
-c=list(a)
-b=list(b)
 
-for i in a:
-    for j in b:
-        print(i,j)
-        if i==j:
-            c.remove(i)
-            b.remove(j)
-            break
-print(c)
-print(b)
-length=len(c)+len(b)
-print(length)
-ans=length%len(d)
-print([d][ans-1])
-'''
-        
+#to read the text document
+
 f=open('Chaith.txt','r')
 print(f.read())
 
+#to read the words in the text document
+
+f=open('Chaith.txt','r')
+for i in f:
+    for j in i.split():
+        print(j)
+        
+#to write in the text document
+a=open('Chaith.txt','w')
+a.write('\n Hi Cutie')
+a.close()
+
+#to append something into the text document
+
+a=open('Chaith.txt','a')
+a.write('\nYou are Cute')
+a.close()
+
+# to write username and password into the text document until we write exit
+while True:
+    user=input('enter a username')
+   
+    if user=='exit':
+        break
+    password=input('enter a password')
+    a=open('Chaith.txt','a')
+    a.write('\n user {}: password {}'.format(user,password))
+a.close()
+
+#to replace bita by tata and then to paste into a new document
+a=open('Chaith.txt','r')
+x=open('Chaithu1.txt','a')
+for i in a:
+    
+    if 'bita' in i:
+        i=i.replace('bita','tata')
+        
+        x.write(i)
+    else:
+        x.write(i )
+x.close()        
+                   
+                      
+        
+    
